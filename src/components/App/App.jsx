3c65b12fb6f59b640e3cba745812f9091fcf8930
Boris <91095836+BorisKorsun@users.noreply.github.com>
2023-05-15 14:49:57 +0200

    adds localStorage fix

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,15 +6,39 @@ import PhonebookForm from "components/PhonebookForm";
 import ContactList from "components/ContactList";
 import Filter from "components/Filter";
 
+const defaultContacts = [
+    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+];
+
 class App extends Component {
     state = {
-        contacts: [
-            {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-            {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-            {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-            {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-    ],
+        contacts: defaultContacts,
         filter: '',
+    };
+
+    componentDidMount() {
+        console.log('mount');
+        const savedContacts = JSON.parse(localStorage.getItem('contacts'))
+
+        if (!savedContacts) {
+            return;
+        };
+
+        this.setState({
+            contacts: savedContacts,
+        });
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        const nextContacts = this.state.contacts;
+        const prevContacts = prevState.contacts;
+
+        if (prevContacts !== nextContacts) {
+            return localStorage.setItem('contacts', JSON.stringify(nextContacts))
+        }
     };
 
     handleSubmit = ({ name, number }) => {
